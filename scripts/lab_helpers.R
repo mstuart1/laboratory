7720a396ecf9ebf3b3ebf031830b28fd14e24b7c
Michelle Stuart <michelle.stuart@rutgers.edu>
2017-09-27 16:16:12 -0400

    init

---
+++ scripts/lab_helpers.R
@@ -0,0 +1,32 @@
+# lab helpers - helper functions for lab work
+
+# read_db ####
+#' views all of the fish recaptured at a given site
+#' @export
+#' @name read_db
+#' @author Michelle Stuart
+#' @param x = which db?
+#' @examples 
+#' db <- read_Db("Leyte")
+
+read_db <- function(x){
+  library(dplyr)
+  db <- src_mysql(dbname = x, default.file = path.expand("~/myconfig.cnf"), port = 3306, create = F, host = NULL, user = NULL, password = NULL)
+  return(db)
+}
+
+# write_db ####
+#' access db with intent to change it
+#' @export
+#' @name write_db
+#' @author Michelle Stuart
+#' @param x = which db?
+#' @examples 
+#' db <- write_db("Leyte")
+
+write_db <- function(x){
+  library(RMySQL)
+  db <- dbConnect(MySQL(), dbname = x, default.file = path.expand("~/myconfig.cnf"), port = 3306, create = F, host = NULL, user = NULL, password = NULL)
+  return(db)
+}
+
