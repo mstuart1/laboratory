cc41d18277972f2f0fd797e02beaf5cc2fd19303
Michelle Stuart <michelle.stuart@rutgers.edu>
2017-10-11 14:05:31 -0400

    add make_plate_with_negs

--- scripts/lab_helpers.R
+++ scripts/lab_helpers.R
@@ -9,9 +9,9 @@
 #' @examples 
 #' db <- read_Db("Leyte")
 
-read_db <- function(x){
+read_db <- function(db_name){
   library(dplyr)
-  db <- src_mysql(dbname = x, default.file = path.expand("~/myconfig.cnf"), port = 3306, create = F, host = NULL, user = NULL, password = NULL)
+  db <- src_mysql(dbname = db_name, default.file = path.expand("~/myconfig.cnf"), port = 3306, create = F, host = NULL, user = NULL, password = NULL)
   return(db)
 }
 
@@ -24,9 +24,9 @@ read_db <- function(x){
 #' @examples 
 #' db <- write_db("Leyte")
 
-write_db <- function(x){
+write_db <- function(db_name){
   library(RMySQL)
-  db <- dbConnect(MySQL(), dbname = x, default.file = path.expand("~/myconfig.cnf"), port = 3306, create = F, host = NULL, user = NULL, password = NULL)
+  db <- dbConnect(MySQL(), dbname = db_name, default.file = path.expand("~/myconfig.cnf"), port = 3306, create = F, host = NULL, user = NULL, password = NULL)
   return(db)
 }
 
@@ -41,105 +41,102 @@ write_db <- function(x){
 #' @examples 
 #' plate <- plate_from_db(todo, extraction_id)
 
-plate_from_db <- function(x, y){
+plate_from_db <- function(table_name, id_type){
   # split the well out into row and column 
-  x$row <- substr(x$well, 1, 1)
-  x$col <- as.numeric(substr(x$well, 2, 3))
+  table_name$row <- substr(table_name$well, 1, 1)
+  table_name$col <- as.numeric(substr(table_name$well, 2, 3))
   
   # select columns for plate 
-  if (y == "extraction_id"){
-    x <- x %>% 
-      select(row, col, extraction_id) %>% #keep row & col, identifier
+  if (id_type == "extraction_id"){
+    table_name <- table_name %>% 
+      select(row, col, id_type) %>% #keep row & col, identifier
       arrange(row, col)
   }
-  if (y == "sample_id"){
-    x <- x %>% 
+  if (id_type == "sample_id"){
+    table_name <- table_name %>% 
       select(row, col, sample_id) %>% #keep row & col, identifier
       arrange(row, col)
   }
-  if (y == "digest_id"){
-    x <- x %>% 
+  if (id_type == "digest_id"){
+    table_name <- table_name %>% 
       select(row, col, digest_id) %>% #keep row & col, identifier
       arrange(row, col)
   }
-  if (y == "ligation_id"){
-    x <- x %>% 
+  if (id_type == "ligation_id"){
+    table_name <- table_name %>% 
       select(row, col, ligation_id) %>% #keep row & col, identifier
       arrange(row, col)
   }
 
-  x <- as.data.frame(x)
+  table_name <- as.data.frame(table_name)
   
   # make map
-  platemap <<- as.matrix(reshape2::acast(x,x[,1] ~ x[,2]))
-  return(x)
+  platemap <<- as.matrix(reshape2::acast(table_name,table_name[,1] ~ table_name[,2]))
+  return(table_name)
 }
 
 
-
-
-
-
-
-
-
-
-
-# make_plate ####
-#' access db with intent to change it
+# make_plate_with_negs ####
+#' turn a table into plates with negative controls
 #' @export
-#' @name make_plate
+#' @name make_plate_with_negs
 #' @author Michelle Stuart
-#' @param x = the table of samples to turn into a plate
+#' @param x = list of ids
+#' @param y = id_type
 #' @examples 
-#' table <- make_plate(todo)
+#' plate <- make_plate_with_negs(todo)
 
-# make_plate <- function(x){  ########## IN PROGRESS ################
-#   # how many rows are in the table (how many samples)?
-#   y <- nrow(x)
-#   
-#   # how many plates would these make, 94 samples plus 2 blanks per plate
-#   (nplates <- floor(y/94)) # extra parenthesis are to print
-#   
-#   # define wells
-#   well <- 1:(96*nplates)
-#   
-#   # separate list of samples out into plates
-#   
-#   # insert the negative controls
-#   a <- (nrow(todo)+1)
-#   todo[a, ] <- "XXXX"
-#   
-#   extr <- data.frame()
-#   for (i in 1:nplates){
-#     c <- 94*i-93 # well 1 on a plate
-#     d <- 94*i-83 # 11
-#     e <- 94*i-82 # 12 negative control well
-#     f <- 94*i-81 # 13
-#     g <- 94*i-34 # 60
-#     h <- 94*i-33 # 61 negative control well
-#     j <- 94*i-32 # 62
-#     k <- 94*i + 2 # 96
-#     l <- 94*i - 35 # 59
-#     m <- 94 * i #94
-#     str1 <- as.data.frame(cbind(well[c:d], todo[c:d,])) # 1:11
-#     names(str1) <- c("well", "sample_id")
-#     str2 <- as.data.frame(cbind(well[e], todo[a,])) # because the first blank is in the 12th position
-#     names(str2) <- c("well", "sample_id")
-#     str3 <- as.data.frame(cbind(well[f:g], todo[e:l,])) #13:60 in plate, 12:59 in list
-#     names(str3) <- c("well", "sample_id")
-#     str4 <- as.data.frame(cbind(well[h], todo[a,])) # because the 2nd blank is in the 61st position
-#     names(str4) <- c("well", "sample_id")
-#     str5 <- as.data.frame(cbind(well[j:k], todo[g:m,]))# 62:96 in plate, 60:94 in list
-#     names(str5) <- c("well", "sample_id")
-#     
-#     # and stick all of the rows together
-#     temp <- data.frame(rbind(str1, str2, str3, str4, str5))
-#     temp$Row <- rep(LETTERS[1:8], 12)
-#     temp$Col <- unlist(lapply(1:12, rep, 8))
-#     temp$plate <- paste("plate", i, sep = "")
-#     extr <- rbind(extr, temp)
-#     
-#   }
-#   
-# }
+make_plate_with_negs <- function(list_of_ids, id_type){
+  # make a dataframe of the list_of_ids
+  list_of_ids <- as.data.frame(list_of_ids)
+  
+  # how many rows are in the table (how many samples)?
+  y <- nrow(list_of_ids)
+
+  # how many plates would these make, 94 samples plus 2 blanks per plate
+  (nplates <- floor(y/94)) # extra parenthesis are to print
+
+  # define wells
+  well <- 1:(96*nplates)
+
+  # insert the negative controls and set up the plate
+  plate <- data.frame() # blank data frame to build upon
+  for (i in 1:nplates){
+    c <- 94*i-93 # well 1 on a plate
+    d <- 94*i-83 # 11
+    e <- 94*i-82 # 12 negative control well
+    f <- 94*i-81 # 13
+    g <- 94*i-34 # 60
+    h <- 94*i-33 # 61 negative control well
+    j <- 94*i-32 # 62
+    k <- 94*i + 2 # 96
+    l <- 94*i - 35 # 59
+    m <- 94 * i #94
+    str1 <- as.data.frame(cbind(well[c:d], list_of_ids[c:d,])) # 1:11
+    names(str1) <- c("well", "id_type")
+    str2 <- as.data.frame(cbind(well[e], "XXXX")) # because the first blank is in the 12th position
+    names(str2) <- c("well", "id_type")
+    str3 <- as.data.frame(cbind(well[f:g], list_of_ids[e:l,])) #13:60 in plate, 12:59 in list
+    names(str3) <- c("well", "id_type")
+    str4 <- as.data.frame(cbind(well[h], "XXXX")) # because the 2nd blank is in the 61st position
+    names(str4) <- c("well", "id_type")
+    str5 <- as.data.frame(cbind(well[j:k], list_of_ids[g:m,]))# 62:96 in plate, 60:94 in list
+    names(str5) <- c("well", "id_type")
+    
+    # and stick all of the rows together
+    temp <- data.frame(rbind(str1, str2, str3, str4, str5))
+    temp$Row <- rep(LETTERS[1:8], 12)
+    temp$Col <- unlist(lapply(1:12, rep, 8))
+    temp$plate <- paste("plate", i, sep = "")
+    plate <- rbind(plate, temp)
+    
+  }
+  
+  # put the samples in order of id (with negative controls inserted)
+  plate <- arrange(plate, plate, Col, Row)
+  
+  return(plate)
+
+  
+
+}
