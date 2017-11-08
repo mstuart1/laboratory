08be8e5187d8807995d6c74ef8781315c94ac4cc
Michelle Stuart <michelle.stuart@rutgers.edu>
2017-11-08 09:46:10 -0500

    working

--- scripts/pltrdr_to_db.R
+++ scripts/pltrdr_to_db.R
@@ -1,18 +1,19 @@
 # Read in quantification results ------------------------------------------
 library(dplyr)
 library(RMySQL)
+library(stringr)
 source("scripts/lab_helpers.R")
 
 # what types of samples are these
-table = "extraction"
-# table = "digest"
+# table = "extraction"
+table = "digest"
 # table = "ligation"
 
 # which plate is it?
-range = "E4195-E4288"
+range = "D3820-D3915"
 
 # read in plate reader data for the first plate
-platefile1 = "/Users/macair/Google Drive/Pinsky Lab/Plate Reader/2017_10/2017-10-09-plate12.txt"
+platefile1 = "data/2017-11-07-plate06.txt"
 # colsinplate1 = 2:12 # is this a full plate?
 
 # skip the lines before the data
@@ -40,10 +41,9 @@ plate <- dat %>%
 
 quant1 <- dplyr::left_join(dat1, plate, by = c("Wells" = "well"))
 quant1 <- quant1 %>% 
-  ### THIS DEPENDS ON THE TYPE OF SAMPLE YOU ARE LOOKING AT extraction_id, digest_id, etc) ###
   select(contains("id"), AdjConc)
 
-# # remove any empty wells and rename the column
+# remove any empty wells and rename the column
 quant1 <- quant1 %>%
 #   filter(!is.na(1)) %>% 
   rename(quant = AdjConc)
@@ -55,29 +55,23 @@ quant1 <- quant1 %>%
 
 # select the samples that will be changed
 change <- dat %>%
-  filter(plate == range)
-
-# remove the samples that will be changed from the main group
-whole <- anti_join(dat, change, by = "extraction_id")
-
-# incorporate the quantification
-change <- change %>% 
+  filter(plate == range) %>% 
   select(-quant)
 
-change <- left_join(change, quant1, by = c("extraction_id", "sample_id"))
+# add in the new quants
+change <- left_join(change, quant1, by = c("digest_id", "extraction_id"))
 
-# add the changes back to the group
-whole <- rbind(whole, change)
+dat <- change_rows(dat, change, "digest_id")
 
 # write the group back to the database
-# dbWriteTable(lab, table, whole, row.names = F, overwrite = T)
+# dbWriteTable(lab, table, dat, row.names = F, overwrite = T)
 # dbDisconnect(lab)
 
 
 # import the firsts #### 
 # this is for the first column of each plate that was put onto a separate plate to make room for the standards
 
-platefile2 = "/Users/macair/Google Drive/Pinsky Lab/Plate Reader/2017_10/2017-10-09-plate14.txt"
+platefile2 = "data/2017-11-07-plate07.txt"
 
 strs <- readLines(platefile2, skipNul = T)
 linestoskip = (which(strs == "Group: Unknowns")) # the number of lines to skip
@@ -87,36 +81,40 @@ dat2 <- read.table(text = strs,  skip = linestoskip, sep = "\t", fill = T, heade
 # remove footer rows
 dat2 <- dat2[1:(which(dat2$Sample == "Group Summaries")-1), ]
 
-# open the list of first column samples
-firsts <- read.csv("output/2017-10-09_firsts_list.csv")
-firsts <- firsts %>% 
-  mutate(Wells = paste(row, col, sep = ""))
+# pull first column samples from database
+firsts <- dat %>% 
+  filter(plate == range, is.na(quant)) %>% 
+  mutate(well = str_replace(well, "1", "7")) %>% # replace the 1 with whatever column of the firsts plate
+  select(digest_id, well)
 
 # read in names for the samples
-quant2 <- dplyr::left_join(dat2, firsts, by = "Wells")
+quant2 <- left_join(dat2, firsts, by = c("Wells" = "well"))
 quant2 <- quant2 %>%
   ### THIS DEPENDS ON THE TYPE OF SAMPLE YOU ARE LOOKING AT extraction_id, digest_id, etc) ###
   select(contains("id"), AdjConc) %>% 
-  rename(quant = AdjConc)
+  rename(quant = AdjConc) %>% 
+  filter(!is.na(digest_id))
 
 # import into database
 lab <- write_db("Laboratory")
-exist <- dbReadTable(lab, table)
+dat <- dbReadTable(lab, table)
 
-# select the rows for extraction ids that are in this quantification and remove the quant column
-change <- exist %>% 
-  filter(extraction_id %in% quant2$extraction_id) %>% 
+# select the samples that will be changed
+change <- dat %>%
+  filter(digest_id %in% quant2$digest_id) %>% 
   select(-quant)
 
-# remove changed rows from exist
-exist <- anti_join(exist, change, by = "extraction_id")
-  
-# add the quantifications to 
-change <- left_join(change, quant2, by = "extraction_id")
+# add in the new quants
+change <- left_join(change, quant2, by = "digest_id")
 
-# replace the rows in exist
-exist <- rbind(exist, change)
+dat <- change_rows(dat, change, "digest_id")
 
-# add back to database
-# dbWriteTable(lab, table, exist, row.names = F, overwrite = T)
+change <- dat %>%
+  filter(plate == range) %>% 
+  mutate(final_vol = 40)
+
+dat <- change_rows(dat, change, "digest_id")
+
+# write the group back to the database
+# dbWriteTable(lab, table, dat, row.names = F, overwrite = T)
 # dbDisconnect(lab)
