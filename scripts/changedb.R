81237cd4ed483393fd4cde414baf2a3ad1f13938
Michelle Stuart <michelle.stuart@rutgers.edu>
2017-11-08 09:46:28 -0500

    add one column of plate reader results

--- scripts/changedb.R
+++ scripts/changedb.R
@@ -8,53 +8,98 @@ lab <- write_db("Laboratory")
 # #for this iteration this script, I am going to change the notes on a group of digests
 # 
 # # pull in all table
-# digs <- lab %>% dbReadTable("digest") %>% collect()
+digs <- lab %>% dbReadTable("digest") %>% collect()
 # 
-# # which(duplicated(extr$extraction_id) == T) # a test to see if any extraction ids are duplicated
 # 
-# change <- digs %>% 
-#   filter(plate == "D4108-D4203") %>%
-#   # filter(extraction_id >= "E4289") %>% 
-#   # mutate(notes = "remaining fin clips don't fit into a set of 2 96 well plates, plan alternative") %>% 
-#   # mutate(plate = NA)
-#   mutate(date = "2017-10-17", notes = NA)
-#   # mutate(notes = NA)
+change <- digs %>%
+  # filter(plate == "D4108-D4203") %>%
+  filter(digest_id >= "D3340" & digest_id < "D3724") %>% 
+  # filter(extraction_id >= "E4289") %>%
+  # mutate(notes = "remaining fin clips don't fit into a set of 2 96 well plates, plan alternative") %>%
+  # mutate(plate = NA)
+  mutate(date = "2017-10-19", notes = NA)
+  # mutate(notes = NA)
 # 
-# digs <- change_rows(digs, change, "digest_id")
+digs <- change_rows(digs, change, "digest_id")
 
 # write the changes to the db
 ################### BE CAREFUL ########################################
-# dbWriteTable(lab, "extraction", extr, row.names = F, overwrite = T)
-# 
-# dbDisconnect(lab)
-# rm(lab)
+dbWriteTable(lab, "digest", digs, row.names = F, overwrite = T)
+
+dbDisconnect(lab)
+rm(lab)
 
 
-# #for this iteration this script, I am going to change the notes on a group of extractions
-# 
-# # pull in all extractions
-extr <- lab %>% dbReadTable("extraction") %>% collect()
-# 
-# # which(duplicated(extr$extraction_id) == T) # a test to see if any extraction ids are duplicated
-# 
-change <- extr %>%
-  filter(plate == "E0151-E0246") %>%
-  # filter(extraction_id >= "E4289") %>%
-  mutate(notes = paste("empty - ", notes, sep = ""))
-  # mutate(plate = NA)
-  # mutate(date = "2017-10-03", notes = NA)
-# mutate(notes = NA)
-# 
-# 
-# # remove those few from the whole group (the extraction ids are for prechange rows)
-extr <- anti_join(extr, change, by = "extraction_id")
-# 
-# # add in the changed rows
-extr <- rbind(extr, change)
-# 
-# # write the changes to the db
-# ################### BE CAREFUL ########################################
-# # dbWriteTable(lab, "extraction", extr, row.names = F, overwrite = T)
+# # #for this iteration this script, I am going to change the notes on a group of extractions
 # # 
-# # dbDisconnect(lab)
-# # rm(lab)
+# # # pull in all extractions
+# extr <- lab %>% dbReadTable("extraction") %>% collect()
+# # 
+# # # which(duplicated(extr$extraction_id) == T) # a test to see if any extraction ids are duplicated
+# # 
+# change <- extr %>%
+#   filter(plate == "E0151-E0246") %>%
+#   # filter(extraction_id >= "E4289") %>%
+#   mutate(notes = paste("empty - ", notes, sep = ""))
+#   # mutate(plate = NA)
+#   # mutate(date = "2017-10-03", notes = NA)
+# # mutate(notes = NA)
+# # 
+# # 
+# # # remove those few from the whole group (the extraction ids are for prechange rows)
+# extr <- anti_join(extr, change, by = "extraction_id")
+# # 
+# # # add in the changed rows
+# extr <- rbind(extr, change)
+# # 
+# # # write the changes to the db
+# # ################### BE CAREFUL ########################################
+# # # dbWriteTable(lab, "extraction", extr, row.names = F, overwrite = T)
+# # # 
+# # # dbDisconnect(lab)
+# # # rm(lab)
+
+
+# add one column of quants into database
+platefile2 = "data/2017-11-07-plate07.txt"
+
+strs <- readLines(platefile2, skipNul = T)
+linestoskip = (which(strs == "Group: Unknowns")) # the number of lines to skip
+
+dat2 <- read.table(text = strs,  skip = linestoskip, sep = "\t", fill = T, header = T, stringsAsFactors = F)
+
+# remove footer rows
+dat2 <- dat2[1:(which(dat2$Sample == "Group Summaries")-1), ]
+dat2 <- select(dat2, Wells, AdjConc) %>% 
+  rename(quant = AdjConc) %>% 
+  rename(well = Wells)
+
+# pull first column samples from database
+change <- dbReadTable(lab, "extraction") %>%
+  filter(grepl("E3819", plate), grepl("6", well)) %>% 
+  mutate(well = str_replace(well, "6", "8")) %>% 
+  select(-quant)
+
+# read in names for the samples
+quant2 <- left_join(dat2, change, by = "well")
+quant2 <- quant2 %>%
+  ### THIS DEPENDS ON THE TYPE OF SAMPLE YOU ARE LOOKING AT extraction_id, digest_id, etc) ###
+  filter(!is.na(extraction_id)) %>% 
+  select(extraction_id, quant)
+
+table = "extraction"
+# import into database
+lab <- write_db("Laboratory")
+dat <- dbReadTable(lab, table)
+
+# select the samples that will be changed
+change <- dat %>% 
+  filter(extraction_id %in% quant2$extraction_id) %>% 
+  select(-quant)
+
+change <- left_join(change, quant2, by = "extraction_id")
+
+dat <- change_rows(dat, change, "extraction_id")
+
+# write the group back to the database
+# dbWriteTable(lab, table, dat, row.names = F, overwrite = T)
