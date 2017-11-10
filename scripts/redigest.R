ec9f2e76a43aa8a93ca7ac8f404fb9ab8e064e98
Michelle Stuart <michelle.stuart@rutgers.edu>
2017-11-09 22:54:09 -0500

    finishing plate 1

--- scripts/redigest.R
+++ scripts/redigest.R
@@ -31,9 +31,9 @@ for (i in 1:nrow(platelist1)){
   dev.off()
 }
 
-##############################################
+####################################################################
 
-# create digest numbers for these samples
+# create digest numbers for these samples ####
 sub <- extr %>% 
   filter(extraction_id == "E0537" | extraction_id == "E0538" | extraction_id == "E0761") %>% 
   mutate(dig_plate = "redigest1", 
@@ -62,10 +62,63 @@ redigs <- rbind(redigs, sub)
 sub <- extr %>% 
   filter(plate == "E1303-E1398" & extraction_id != "E1341") %>% 
   mutate(dig_plate = "redigest1", 
-         dig_well = c("G5", "D5", "D7", "D11", "E11", "F11", "G11", "G7", "B10"))
+         dig_well = c("C5", "D5", "D7", "D11", "F11", "G11", "H11","G7", "B10"))
 redigs <- rbind(redigs, sub)
 sub <- extr %>% 
-  filter(plate == "E2642-E2737" & !extraction_id %in% c("E2705", "E2713", "E2724", "E2726", "E2728") %>% 
+  filter(plate == "E2642-E2737" & !extraction_id %in% c("E2705", "E2713", "E2724", "E2726", "E2728")) %>% 
   mutate(dig_plate = "redigest1", 
-         dig_well = c("G5", "D5", "D7", "D11", "E11", "F11", "G11", "G7", "B10"))
+         dig_well = c("F6", "G6", "C9", "B8", "C8", "D8", "E8", "G9", "H9", "F10", "C11", "D12", "A12", "B12", "G12"))
 redigs <- rbind(redigs, sub)
+# double check that there are no duplicate wells
+test <- redigs %>% 
+  group_by(dig_well) %>% 
+  summarise(count = n()) %>% 
+  filter(count > 1)
+
+# formatting
+redigs <- redigs %>% 
+  mutate(row = substr(dig_well, 1, 1), 
+         col = substr(dig_well, 2, 3)) %>% 
+  arrange(as.numeric(col), row)
+redigs <- mutate(redigs, digest_id = 1:nrow(redigs))
+
+#################################################################
+
+# send the digests to the database ####
+lab <- write_db("Laboratory")
+
+digs <- dbReadTable(lab, "digest")
+dig_max <- digs %>% 
+  summarise(x = max(digest_id)) %>% 
+  mutate(x = substr(x, 2, 5))
+
+redigs <- redigs %>% 
+  mutate(digest_id = digest_id + as.numeric(dig_max$x)) %>% 
+  mutate(digest_id = paste("D", digest_id, sep = "")) %>% 
+  select(digest_id, extraction_id, dig_well)
+min_dig <- redigs %>% 
+  summarize(x = min(digest_id))
+max_dig <- redigs %>% 
+  summarize(y = max(digest_id))
+
+redigs <- redigs %>% 
+  mutate(
+    date = "2017-11-09", 
+    vol_in = 30, 
+    ng_in = NA, 
+    enzymes = "PstI-MluCI",
+    final_vol = NA, 
+    quant = NA, 
+    notes = NA,
+    correction = NA, 
+    corr_message = NA, 
+    corr_editor = NA, 
+    corr_date = NA, 
+    plate = paste(min_dig, "-", max_dig, sep = "")
+  ) %>% 
+  rename(well = dig_well)
+
+digs <- rbind(digs, redigs)
+
+# dbWriteTable(lab, "digest", digs, row.names = F, overwrite = T)
+
