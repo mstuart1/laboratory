88092533712650b36185f68050f9ad2f3e996fbd
Michelle Stuart <michelle.stuart@rutgers.edu>
2017-11-17 11:42:14 -0500

    add extraction id to columns, remove filter date

--- scripts/partial_plate.R
+++ scripts/partial_plate.R
@@ -6,9 +6,9 @@ lab <- read_db("Laboratory")
 
 digs <- lab %>% 
   tbl("digest") %>% 
-  filter(date == "2017-11-09") %>% 
+  # filter(date == "2017-11-09") %>% 
   arrange(digest_id) %>% 
-  select(digest_id, well, plate) %>% 
+  select(digest_id, well, plate, extraction_id) %>% 
   collect()
 
 # make a list of plate names
@@ -21,7 +21,7 @@ blank$well <- paste(blank$row, blank$col, sep = "")
 
 # separate out 1 plate from digs
 current <- digs %>% 
-  filter(plate == plates$plate[6])
+  filter(plate == "D4204-D4299")
 
 full <- left_join(blank, current, by = "well") %>% 
   select(row, col, digest_id)
@@ -29,4 +29,12 @@ full <- left_join(blank, current, by = "well") %>%
 # make a plate map
 platemap <<- as.matrix(reshape2::acast(full, full$row ~ full$col))
 
+# make a source map
+source <- current %>% 
+  select(well, extraction_id)
+
+fulls <- left_join(blank, source, by = "well") %>% 
+  select(row, col, extraction_id)
+
+platemap <<- as.matrix(reshape2::acast(fulls, fulls$row ~ fulls$col))
   
