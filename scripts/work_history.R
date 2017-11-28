31ba13e9354fa485a1c2dc27545fc72f2a0a8293
Michelle Stuart <michelle.stuart@rutgers.edu>
2017-11-28 08:09:24 -0500

    working with item list

--- scripts/work_history.R
+++ scripts/work_history.R
@@ -2,7 +2,8 @@
 
 source("scripts/lab_helpers.R")
 
-item <- "E1872"
+# item <- regeno
+item <- c("L0304","L0305","L0306","L0308","L0309","L0310","L0313","L0332")
 
 lab <- read_db("Laboratory")
 
@@ -41,10 +42,94 @@ if (grepl("E", item)){
   info <- left_join(info, temp, by = "digest_id")
 }
 
+if (grepl("D", item)){
+  info <- try(lab %>% 
+                tbl("digest") %>% 
+                filter(digest_id %in% item$digest_id | digest_id == item)) %>% 
+    collect()
+  if("try-error" %in% class(info)){
+    info <- lab %>%
+      tbl("digest") %>% 
+      filter(digest_id == item) %>% 
+      collect()
+  }else{
+    info <- lab %>% 
+      tbl("digest") %>% 
+      filter(digest_id %in% item$digest_id) %>% 
+      collect()
+  }
+  
+  info <- info %>% 
+    select(-contains("corr"), -contains("vol"), -ng_in) %>% 
+    rename(dig_date = date,
+           dig_quant = quant, 
+           dig_well = well, 
+           dig_plate = plate, 
+           dig_notes = notes)
+  
+  temp <- try(lab %>% 
+    tbl("extraction") %>% 
+    filter(extraction_id %in% info$extraction_id ) %>% 
+    collect())
+  if("try-error" %in% class(temp)){
+    temp <- lab %>% 
+      tbl("extraction") %>% 
+      filter(extraction_id == info$extraction_id[1]) %>% 
+      collect()
+  }else{
+    temp <- lab %>% 
+      tbl("extraction") %>% 
+      filter(extraction_id %in% info$extraction_id ) %>% 
+      collect()
+  }
+  
+  temp <- temp %>%
+    select(-final_vol, -contains("corr")) %>% 
+    rename(extr_date = date, 
+           extr_quant = quant, 
+           extr_well = well,
+           extr_plate = plate, 
+           extr_notes = notes)
+  
+  info <- left_join(info, temp, by = "extraction_id")
+  
+  temp <- try(lab %>% 
+                tbl("ligation") %>% 
+                filter(digest_id %in% item$digest_id) %>% 
+                collect())
+  if("try-error" %in% class(temp)){
+    temp <- lab %>%
+      tbl("ligation") %>% 
+      filter(digest_id == item) %>% 
+      collect()
+  }else{
+    temp <- lab %>% 
+      tbl("ligation") %>% 
+      filter(digest_id %in% item$digest_id) %>% 
+      collect()
+  }
+  
+  info <- left_join(info, temp, by = "digest_id")
+}
+
 if (grepl("L", item)){
-  info <- lab %>% 
+  info <- try(lab %>% 
     tbl("ligation") %>% 
-    filter(ligation_id == item) %>% 
+    filter(ligation_id %in% item) %>% 
+    collect())
+  if("try-error" %in% class(info)){
+    info <- lab %>%
+      tbl("ligation") %>% 
+      filter(ligation_id == item)%>% 
+      collect()
+  }else{
+    info <- lab %>% 
+      tbl("ligation") %>% 
+      filter(ligation_id %in% item) %>% 
+      collect()
+  }
+  
+  info <- info %>% 
     collect() %>% 
     select(-contains("corr"), -vol_in, -regeno, -water) %>% 
     rename(lig_date = date,
