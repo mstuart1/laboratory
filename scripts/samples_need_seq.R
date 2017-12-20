5ff805cf0a5249b1d49dcea725fc3faac2b5b91b
Michelle Stuart <michelle.stuart@rutgers.edu>
2017-12-20 08:43:33 -0500

    finish samples need seq

--- scripts/samples_need_seq.R
+++ scripts/samples_need_seq.R
@@ -1,12 +1,9 @@
-# samples that need to be sequenced 
-# library(dbplyr)
+# samples that haven't been sequenced "well"
+# they might've been sequenced but didn't pass read or dDocent filters
+
 source("../genomics/scripts/gen_helpers.R")
 leyte <- read_db("Leyte")
 lab <- read_db("Laboratory")
-
-
-
-# samples that have been collected but not extracted
 
 # all the samples we have ever collected
 samples <- leyte %>% 
@@ -16,50 +13,78 @@ samples <- leyte %>%
   arrange(sample_id) %>% 
   collect()
 
-# for each sample, did we extract it, 
-# and if we extracted it more than once, 
-# which extraction has the highest quantity?
+# get work history on each sample
 extr <- lab %>% 
   tbl("extraction") %>% 
-  filter(sample_id %in% samples$sample_id) %>% 
-  select(sample_id, extraction_id, quant) %>% 
-  rename(extr_quant = quant) %>% 
+  select(sample_id, extraction_id) %>% 
   collect()
+
 samples <- left_join(samples, extr, by = "sample_id")
-need_extr <- anti_join(samples, extr, by = "sample_id") 
-samples <- anti_join(samples, need_extr, by = "sample_id")
+rm(extr)
 
+not_extr <- filter(samples, is.na(extraction_id))
 
-multi <- samples %>% # these are the 2012 testing phase samples
-  group_by(sample_id) %>% 
-  summarize(extractions = n()) %>% 
-  filter(extractions > 1)
+# remove un-extracted samples from work list
+samples <- anti_join(samples, not_extr, by = "sample_id")
+# remove extracted samples from not_extr list
+not_extr <- anti_join(not_extr, samples, by = "sample_id")
 
-new <- data.frame()
-for(i in 1:nrow(multi)){
-  x <- samples %>% 
-    filter(sample_id == multi$sample_id[i])
-  x <- x[which.max(x$extr_quant), ]
-  new <- rbind(new, x)
-}
-
-# remove multi from samples
-samples <- samples %>% 
-  filter(!sample_id %in% multi$sample_id)
-# add in the "new" rows
-samples <- rbind(samples, new)
-# these are all samples that have been extracted.  
-
-
-# which extractions were not digested
-digs <- lab %>% 
+dig <- lab %>% 
   tbl("digest") %>% 
-  filter(extraction_id %in% samples$extraction_id) %>% 
-  select(digest_id, extraction_id) %>% 
+  select(extraction_id, digest_id) %>% 
   collect()
-samples <- left_join(samples, digs, by = "extraction_id")
-no_digs <- anti_join(samples, digs, by = "extraction_id")
-samples <- anti_join(samples, no_digs, by = "extraction_id")
 
-# some of the no-digs are in progress, some were never extracted, 
+samples <- left_join(samples, dig, by = "extraction_id")
+rm(dig)
 
+not_dig <- filter(samples, is.na(digest_id))
+
+# remove un-extracted samples from work list
+samples <- anti_join(samples, not_dig, by = "extraction_id")
+# remove extracted samples from not_extr list
+not_dig <- anti_join(not_dig, samples, by = "sample_id")
+
+lig <- lab %>% 
+  tbl("ligation") %>% 
+  select(digest_id, ligation_id, total_reads, retained) %>% 
+  collect()
+
+samples <- left_join(samples, lig, by = "digest_id")
+rm(lig)
+
+not_lig <- filter(samples, is.na(ligation_id))
+
+# remove un-extracted samples from work list
+samples <- anti_join(samples, not_lig, by = "digest_id")
+# remove processes samples from not done list
+not_lig <- anti_join(not_lig, samples, by = "sample_id")
+
+# compare to the genepop
+# locate the genepop file and read as data frame
+genfile <- "../genomics/data/seq17_03g95maf2q30dp15.gen"
+
+genedf <- read_genepop(genfile)
+
+
+# 2) strip any named samples down to pure ligation number ---- 
+# create a search term to search for ligation ids within a name
+ligid <- "(.+)(L\\d\\d\\d\\d)" 
+# change all of the names to ligation id only
+genedf$names <- genedf$names %>% str_replace(ligid,"\\2")
+
+genedf <- genedf %>% 
+  select(names) %>% # remove columns of loci
+  rename(ligation_id=names) %>% 
+  mutate(seq = "yes")
+
+samples <- left_join(samples, genedf, by = "ligation_id")
+rm(genedf)
+
+not_pass <- filter(samples, is.na(seq)) 
+  
+# remove un-extracted samples from work list
+samples <- anti_join(samples, not_pass, by = "ligation_id")
+# remove processes samples from not done list
+not_pass <- anti_join(not_pass, samples, by = "sample_id")
+
+# as of 12-20-2017 it looks like 311 of the not_pass samples are not in progress and could use a look to see if they can be made to work.
