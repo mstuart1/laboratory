0bcde6584fad0352668da44982d16cefb375b75b
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-01-26 16:14:28 -0500

    change notes in ligation table

--- scripts/changedb.R
+++ scripts/changedb.R
@@ -9,13 +9,14 @@ lab <- write_db("Laboratory")
 # #for this iteration this script, I am going to change the notes on a group of digests
 # 
 # # pull in all table
-digs <- lab %>% dbReadTable("digest") %>% collect()
+# digs <- lab %>% dbReadTable("digest") %>% collect()
+ligs <- lab %>% dbReadTable("ligation") %>% collect()
 # 
 # 
-change <- digs %>%
-  filter(extraction_id %in% c("E4132", "E4139", "E4152", "E4157", "E4162", "E1463", "E4164", "E4168", "E4172")) %>% 
-    mutate(vol_in = 15, 
-          ng_in = ng_in/2)
+# change <- digs %>%
+  # filter(extraction_id %in% c("E4132", "E4139", "E4152", "E4157", "E4162", "E1463", "E4164", "E4168", "E4172")) %>% 
+    # mutate(vol_in = 15, 
+          # ng_in = ng_in/2)
   # filter(date == "2015-12-01") %>% 
   # filter(digest_id >= "D3340" & digest_id < "D3724") %>% 
   # filter(extraction_id >= "E4289") %>%
@@ -24,11 +25,16 @@ change <- digs %>%
   # mutate(date = "2017-10-19", notes = NA)
   # mutate(notes = NA)
 # 
-digs <- change_rows(digs, change, "digest_id")
 
+change <- ligs %>% 
+  mutate(notes = ifelse(ligation_id >= "L3171", "ligation planned for January 2018", notes))
+
+# digs <- change_rows(digs, change, "digest_id")
+ligs <- change_rows(ligs, change, "ligation_id")
 # write the changes to the db
 ################### BE CAREFUL ########################################
 # dbWriteTable(lab, "digest", digs, row.names = F, overwrite = T)
+# dbWriteTable(lab, "ligation", ligs, row.names = F, overwrite = T)
 # 
 # dbDisconnect(lab)
 # rm(lab)
