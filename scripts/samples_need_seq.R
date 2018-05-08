b7ef0e61d5dcaaca9dbbe33d2d63215baff8660b
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-05-08 12:26:41 -0400

    narrows down samples to what needs work

--- scripts/samples_need_seq.R
+++ scripts/samples_need_seq.R
@@ -16,70 +16,27 @@ samples <- leyte %>%
 # get work history on each sample
 extr <- lab %>% 
   tbl("extraction") %>% 
-  select(sample_id, extraction_id) %>% 
+  select(sample_id, extraction_id, quant) %>% 
   collect()
 
-samples <- left_join(samples, extr, by = "sample_id")
+samples <- left_join(samples, extr, by = "sample_id") %>% 
+  rename(extr_quant = quant)
 rm(extr)
-
-# not_extr <- filter(samples, is.na(extraction_id))
-
-# # get notes to see why not extracted
-# temp <- leyte %>%
-#   tbl("clownfish") %>%
-#   filter(sample_id %in% not_extr$sample_id) %>%
-#   select(sample_id, notes) %>%
-#   collect()
-# not_extr <- left_join(not_extr, temp, by = "sample_id")
-# rm(temp)
-# 
-# # remove samples that cannot be extracted
-# not_extr <- not_extr %>%
-#   filter(!grepl("no", notes))
-# 
-# # save list of samples to be extracted
-# save(not_extr, file="data/need_extract.Rdata")
-  
-
-# remove un-extracted samples from work list
-samples <- anti_join(samples, not_extr, by = "sample_id")
-
-# # remove extracted samples from not_extr list
-# not_extr <- anti_join(not_extr, samples, by = "sample_id")
 
 dig <- lab %>% 
   tbl("digest") %>% 
-  select(extraction_id, digest_id) %>% 
+  select(extraction_id, digest_id, quant) %>% 
   collect()
 
-samples <- left_join(samples, dig, by = "extraction_id")
+samples <- left_join(samples, dig, by = "extraction_id") %>% 
+  rename(dig_quant = quant)
 rm(dig)
 
-# not_dig <- filter(samples, is.na(digest_id))
-
-# # get notes to see why not done
-# temp <- lab %>%
-#   tbl("extraction") %>%
-#   filter(extraction_id %in% not_dig$extraction_id) %>%
-#   select(extraction_id, notes) %>%
-#   collect()
-# not_dig <- left_join(not_dig, temp, by = "extraction_id")
-# not_dig <- select(not_dig, -size)
-# rm(temp)
-# 
-# # remove samples that cannot be digested because they are gone or have no band
-# not_dig <- not_dig %>%
-#   filter(extraction_id >= "E0247", 
-#     !grepl("empty", notes), 
-#     !grepl("no band", notes)) 
-# 
-# # save list of samples to be extracted
-# save(not_dig, file="data/need_digest.Rdata")
-
-# remove un-done samples from work list
-# samples <- anti_join(samples, not_dig, by = "extraction_id")
-# remove done samples from not done list
-# not_dig <- anti_join(not_dig, samples, by = "sample_id")
+# # which samples were digested more than once?
+# multi_dig <- samples %>% 
+#   group_by(sample_id) %>% 
+#   summarise(n = n()) %>% 
+#   filter(n > 1)
 
 lig <- lab %>% 
   tbl("ligation") %>% 
@@ -89,12 +46,6 @@ lig <- lab %>%
 samples <- left_join(samples, lig, by = "digest_id")
 rm(lig)
 
-# not_lig <- filter(samples, is.na(ligation_id))
-
-# remove un-extracted samples from work list
-# samples <- anti_join(samples, not_lig, by = "digest_id")
-# remove processed samples from not done list
-# not_lig <- anti_join(not_lig, samples, by = "sample_id")
 
 # compare to the genepop
 # locate the genepop file and read as data frame
@@ -117,21 +68,16 @@ genedf <- genedf %>%
 samples <- left_join(samples, genedf, by = "ligation_id")
 rm(genedf)
 
-not_pass <- filter(samples, is.na(seq)) 
+seq <- samples %>% 
+  filter(seq == "yes")
+not_pass <- samples %>% 
+  filter(!sample_id %in% seq$sample_id)
   
-# # remove un-extracted samples from work list
-# samples <- anti_join(samples, not_pass, by = "ligation_id")
-# # remove processes samples from not done list
-# not_pass <- anti_join(not_pass, samples, by = "sample_id")
-# 
-# # as of 12-20-2017 it looks like 311 of the not_pass samples are not in progress and could use a look to see if they can be made to work.
-
-# get notes for the not_pass samples
+# get notes  for the not_pass samples
 lig_notes <- lab %>% 
   tbl("ligation") %>% 
   select(ligation_id, notes) %>% 
-  collect() %>% 
-  filter(!grepl("failed", notes))
+  collect() 
 not_pass <- left_join(not_pass, lig_notes, by = "ligation_id") %>% 
   rename(lig_notes = notes)
 rm(lig_notes)
@@ -147,33 +93,114 @@ rm(dig_notes)
 extr_notes <- lab %>% 
   tbl("extraction") %>% 
   select(extraction_id, notes) %>% 
-  collect() %>% 
-  filter(extraction_id != "E0726", # sample is empty but notes aren't filtering
-    !grepl("empty", notes), 
-    !grepl("no band", notes), 
-    !grepl("No band", notes))
+  collect() 
+# %>% 
+#   filter(extraction_id != "E0726", # sample is empty but notes aren't filtering
+#     !grepl("empty", notes), 
+#     !grepl("no band", notes), 
+#     !grepl("No band", notes))
 not_pass <- left_join(not_pass, extr_notes, by = "extraction_id") %>% 
   rename(extr_notes = notes)
 rm(extr_notes)
 
+samp_notes <- leyte %>% 
+  tbl("clownfish") %>% 
+  select(sample_id, notes) %>% 
+  collect()
+not_pass <- left_join(not_pass, samp_notes, by = "sample_id") %>% 
+  rename(samp_notes = notes)
+rm(samp_notes)
+
 not_pass <- not_pass %>% 
-  select(sample_id, -size, extraction_id, extr_notes, digest_id, dig_notes, ligation_id, lig_notes, total_reads, retained, seq)
+  select(sample_id, size, samp_notes, extraction_id, extr_quant, extr_notes, digest_id, dig_quant, dig_notes, ligation_id, lig_notes, total_reads, retained, seq)
 
+# # are there any trends between extraction quantification or fish size of samples that passed sequencing and samples that did not?
+# library(ggplot2)
 
-not_pass <- not_pass %>%
-  # remove extractions that can't be digested
-  filter(extraction_id >= "E0247", 
-    extraction_id != "E0726")
+# # is there a size difference between successful samples and not?
+# samples <- samples %>% 
+#   mutate(size = as.numeric(size), 
+#     seq = ifelse(is.na(seq), "no", seq))
+# subset <- samples %>% 
+#   filter(!is.na(ligation_id)) # samples that were passed to sequencer
+# ggplot(data = subset, mapping = aes(x = seq, y = size)) +
+#   geom_boxplot()
+# # not really
 
-# for ligations that are in progress, remove those sample_ids from the list
-lig_prog <- not_pass %>% 
-  filter(grepl("planned", lig_notes), 
-    !grepl("failed", lig_notes)) %>% 
-  select(sample_id)
-not_pass <- anti_join(not_pass, lig_prog, by = "sample_id")
-rm(lig_prog)
+# # # is there a extr_quant difference between successful samples and not?
+# samples <- samples %>%
+#   mutate(seq = ifelse(is.na(seq), "no", seq))
+# subset <- samples %>%
+#   filter(!is.na(ligation_id)) # samples that were passed to sequencer
+# ggplot(data = subset, mapping = aes(x = seq, y = extr_quant)) +
+#   geom_boxplot()
+# # not really
+# # what about all samples?
+# ggplot(data = samples, mapping = aes(x = seq, y = extr_quant)) +
+#   geom_boxplot()
+# # get rid of high outliers to zoom in on box
+# subset <- samples %>% 
+#   filter(!is.na(extr_quant), 
+#     extr_quant < 150)
+# ggplot(data = subset, mapping = aes(x = seq, y = extr_quant)) +
+#   geom_boxplot()
+# # no difference
 
-# which of these samples have not been digested? ####
-no_dig <- not_pass %>% 
-  filter(is.na(digest_id)) %>% 
-  arrange(sample_id)
+  # not_pass <- not_pass %>%
+#   # remove extractions that can't be digested
+#   filter(extraction_id >= "E0247", 
+#     extraction_id != "E0726")
+# 
+# # for ligations that are in progress, remove those sample_ids from the list
+# lig_prog <- not_pass %>% 
+#   filter(grepl("planned", lig_notes), 
+#     !grepl("failed", lig_notes)) %>% 
+#   select(sample_id)
+# not_pass <- anti_join(not_pass, lig_prog, by = "sample_id")
+# rm(lig_prog)
+# 
+# # which of these samples have not been digested? ####
+# no_dig <- not_pass %>% 
+#   filter(is.na(digest_id)) %>% 
+#   arrange(sample_id)
+
+# which not_pass samples are unextracted?
+unextracted <- not_pass %>% 
+  filter(is.na(extraction_id), 
+    !grepl("APCL18", sample_id))
+
+# remove samples that are unextractable (no fin in tube, no EtOH)
+not_pass <- not_pass %>% 
+  filter(!sample_id %in% unextracted$sample_id)
+
+# remove samples that are 2018 collection
+not_pass <- not_pass %>% 
+  filter(!grepl("APCL18", sample_id))
+
+# remove samples that are in progress 
+mid_progress <- not_pass %>% 
+  filter(grepl("plan", lig_notes)| 
+    grepl("plan", dig_notes))
+not_pass <- not_pass %>% 
+  filter(!sample_id %in% mid_progress$sample_id)
+
+# remove samples with no extract left
+not_pass <- not_pass %>% 
+  filter(!grepl("empty", extr_notes))
+
+# remove samples that have been ligated more than once and still haven't succeeded
+multi_lig <- not_pass %>% 
+  filter(!is.na(ligation_id)) %>% 
+  group_by(sample_id) %>% 
+  summarise(count = n()) %>% 
+  filter(count > 1)
+not_pass <- not_pass %>% 
+  filter(!sample_id %in% multi_lig$sample_id)
+
+extr_loc <- lab %>% 
+  tbl("extraction") %>% 
+  select(extraction_id, plate, well, gel) %>% 
+  collect()
+not_pass <- left_join(not_pass, extr_loc, by = "extraction_id")
+
+write.csv(not_pass, file = "data/attempt_to_rescue.csv", row.names = F)
