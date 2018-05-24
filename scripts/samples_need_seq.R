8550007fc3ae3210093e64a17ac46c1d946452fe
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-05-24 11:06:26 -0400

    revamped to create more detailed reports

--- scripts/samples_need_seq.R
+++ scripts/samples_need_seq.R
@@ -1,60 +1,17 @@
-# samples that haven't been sequenced "well"
-# they might've been sequenced but didn't pass read or dDocent filters
+# get work progress on all samples
 
 source("../genomics/scripts/gen_helpers.R")
 leyte <- read_db("Leyte")
 lab <- read_db("Laboratory")
 
-# all the samples we have ever collected
-samples <- leyte %>% 
-  tbl("clownfish") %>% 
-  filter(!is.na(sample_id)) %>% 
-  select(sample_id, size) %>% 
-  arrange(sample_id) %>% 
-  collect()
-
-# get work history on each sample
-extr <- lab %>% 
-  tbl("extraction") %>% 
-  select(sample_id, extraction_id, quant) %>% 
-  collect()
-
-samples <- left_join(samples, extr, by = "sample_id") %>% 
-  rename(extr_quant = quant)
-rm(extr)
-
-dig <- lab %>% 
-  tbl("digest") %>% 
-  select(extraction_id, digest_id, quant) %>% 
-  collect()
-
-samples <- left_join(samples, dig, by = "extraction_id") %>% 
-  rename(dig_quant = quant)
-rm(dig)
-
-# # which samples were digested more than once?
-# multi_dig <- samples %>% 
-#   group_by(sample_id) %>% 
-#   summarise(n = n()) %>% 
-#   filter(n > 1)
-
-lig <- lab %>% 
-  tbl("ligation") %>% 
-  select(digest_id, ligation_id, total_reads, retained) %>% 
-  collect()
-
-samples <- left_join(samples, lig, by = "digest_id")
-rm(lig)
-
-
-# compare to the genepop
+# Sequencing Success ####
+# get all of the samples that have passed the dDocent filters and are considered successfully sequenced
 # locate the genepop file and read as data frame
 genfile <- "../genomics/data/seq17_03g95maf2q30dp15.gen"
 
 genedf <- read_genepop(genfile)
 
-
-# 2) strip any named samples down to pure ligation number ---- 
+# strip any named samples down to pure ligation number ---- 
 # create a search term to search for ligation ids within a name
 ligid <- "(.+)(L\\d\\d\\d\\d)" 
 # change all of the names to ligation id only
@@ -65,54 +22,128 @@ genedf <- genedf %>%
   rename(ligation_id=names) %>% 
   mutate(seq = "yes")
 
-samples <- left_join(samples, genedf, by = "ligation_id")
-rm(genedf)
+# get sample ids for these ligations
+samples <- samp_from_lig(genedf)
 
-seq <- samples %>% 
-  filter(seq == "yes")
-not_pass <- samples %>% 
-  filter(!sample_id %in% seq$sample_id)
-  
-# get notes  for the not_pass samples
-lig_notes <- lab %>% 
+if (nrow(samples) != nrow(genedf)){
+  # are there duplicate ligation ids in genedf?
+  no_dups <- distinct(genedf, ligation_id) 
+    # that wasn't the issue, same number of rows in no_dups as in genedf
+
+  # which ligation ids are in genedf that are not in samples?
+  no_sample <- genedf %>% filter(!ligation_id %in% samples$ligation_id)
+    # these 11 samples are problem samples that can't be matched to a sample
+}else{
+  print("All sequenced samples accounted for")
+}
+
+write.csv(samples, file = paste("data/samples_successfully_seq_as_of_", Sys.Date(), ".csv", sep = "" ))
+
+rm(no_dups, no_sample, genedf)
+
+# Unable to Extract ####
+
+# all the samples we have ever collected
+all_samples <- leyte %>% 
+  tbl("clownfish") %>% 
+  filter(!is.na(sample_id)) %>% 
+  select(sample_id, size) %>% 
+  arrange(sample_id) %>% 
+  collect()
+
+# remove the samples that have successfully been sequenced
+all_samples <- all_samples %>% 
+  filter(!sample_id %in% samples$sample_id)
+
+# get work history on each sample
+extr <- lab %>% 
+  tbl("extraction") %>% 
+  select(sample_id, extraction_id, quant, date) %>% 
+  collect() %>% 
+  rename(extr_date = date)
+
+# which samples have not been extracted?
+no_extr <- all_samples %>% 
+  filter(!sample_id %in% extr$sample_id)
+
+# of the unextracted samples, which are planned to be extracted?
+future_extr <- no_extr %>% 
+  filter(grepl("APCL18", sample_id))
+write.csv(future_extr, file = paste("data/to_be_extracted_as_of_", Sys.Date(), ".csv", sep = ""))
+
+# which can never be extracted?
+no_extr <- anti_join(no_extr, future_extr, by = "sample_id")
+write.csv(no_extr, file = paste("data/unextractable_as_of_", Sys.Date(), ".csv", sep = ""))
+
+# remove those from the list of samples that need work
+samples <- all_samples %>% 
+  filter(!sample_id %in% future_extr$sample_id, 
+    !sample_id %in% no_extr$sample_id)
+
+samples <- left_join(samples, extr, by = "sample_id")
+
+rm(all_samples, extr, future_extr, no_extr)
+
+dig <- lab %>% 
+  tbl("digest") %>% 
+  select(extraction_id, digest_id, quant, date) %>% 
+  collect() %>% 
+  rename(dig_quant = quant, 
+    dig_date = date)
+
+samples <- left_join(samples, dig, by = "extraction_id") 
+rm(dig)
+
+# # which samples were digested more than once?
+# multi_dig <- samples %>% 
+#   group_by(sample_id) %>% 
+#   summarise(n = n()) %>% 
+#   filter(n > 1)
+
+lig <- lab %>% 
   tbl("ligation") %>% 
-  select(ligation_id, notes) %>% 
-  collect() 
-not_pass <- left_join(not_pass, lig_notes, by = "ligation_id") %>% 
+  select(digest_id, ligation_id, total_reads, retained, date) %>% 
+  collect() %>% 
+  rename(lig_date = date)
+
+samples <- left_join(samples, lig, by = "digest_id")
+rm(lig)
+
+# # get notes  for the remaining samples
+lig_notes <- lab %>%
+  tbl("ligation") %>%
+  select(ligation_id, notes) %>%
+  collect()
+samples <- left_join(samples, lig_notes, by = "ligation_id") %>%
   rename(lig_notes = notes)
 rm(lig_notes)
 
-dig_notes <- lab %>% 
-  tbl("digest") %>% 
-  select(digest_id, notes) %>% 
+dig_notes <- lab %>%
+  tbl("digest") %>%
+  select(digest_id, notes) %>%
   collect()
-not_pass <- left_join(not_pass, dig_notes, by = "digest_id") %>% 
+samples <- left_join(samples, dig_notes, by = "digest_id") %>%
   rename(dig_notes = notes)
 rm(dig_notes)
 
-extr_notes <- lab %>% 
-  tbl("extraction") %>% 
-  select(extraction_id, notes) %>% 
-  collect() 
-# %>% 
-#   filter(extraction_id != "E0726", # sample is empty but notes aren't filtering
-#     !grepl("empty", notes), 
-#     !grepl("no band", notes), 
-#     !grepl("No band", notes))
-not_pass <- left_join(not_pass, extr_notes, by = "extraction_id") %>% 
+extr_notes <- lab %>%
+  tbl("extraction") %>%
+  select(extraction_id, notes) %>%
+  collect()
+
+samples <- left_join(samples, extr_notes, by = "extraction_id") %>%
   rename(extr_notes = notes)
 rm(extr_notes)
 
-samp_notes <- leyte %>% 
-  tbl("clownfish") %>% 
-  select(sample_id, notes) %>% 
+samp_notes <- leyte %>%
+  tbl("clownfish") %>%
+  select(sample_id, notes) %>%
   collect()
-not_pass <- left_join(not_pass, samp_notes, by = "sample_id") %>% 
+samples <- left_join(samples, samp_notes, by = "sample_id") %>%
   rename(samp_notes = notes)
 rm(samp_notes)
+ 
 
-not_pass <- not_pass %>% 
-  select(sample_id, size, samp_notes, extraction_id, extr_quant, extr_notes, digest_id, dig_quant, dig_notes, ligation_id, lig_notes, total_reads, retained, seq)
 
 # # are there any trends between extraction quantification or fish size of samples that passed sequencing and samples that did not?
 # library(ggplot2)
@@ -164,43 +195,55 @@ not_pass <- not_pass %>%
 #   filter(is.na(digest_id)) %>% 
 #   arrange(sample_id)
 
-# which not_pass samples are unextracted?
-unextracted <- not_pass %>% 
-  filter(is.na(extraction_id), 
-    !grepl("APCL18", sample_id))
 
-# remove samples that are unextractable (no fin in tube, no EtOH)
-not_pass <- not_pass %>% 
-  filter(!sample_id %in% unextracted$sample_id)
+# remove empty extracts
+empty <- samples %>% 
+  filter(grepl("empty", extr_notes))
 
-# remove samples that are 2018 collection
-not_pass <- not_pass %>% 
-  filter(!grepl("APCL18", sample_id))
+samples <- samples %>% 
+  filter(!sample_id %in% empty$sample_id)
+write.csv(empty, file = paste("data/empty_extracts_as_of_", Sys.Date(), ".csv", sep = ""))
 
-# remove samples that are in progress 
-mid_progress <- not_pass %>% 
-  filter(grepl("plan", lig_notes)| 
-    grepl("plan", dig_notes))
-not_pass <- not_pass %>% 
-  filter(!sample_id %in% mid_progress$sample_id)
 
-# remove samples with no extract left
-not_pass <- not_pass %>% 
-  filter(!grepl("empty", extr_notes))
+# how many samples have a ligation without a date? (planned to ligate) ####
+lig_in_progress <- samples %>% 
+  filter(!is.na(ligation_id) &  # has been planned in a ligation
+    is.na(lig_date)) # has not been ligated
+write.csv(lig_in_progess, file = paste("data/lig_in_progress_as_of_", Sys.Date(), ".csv", sep = ""))
+samples <- samples %>% 
+  filter(!sample_id %in% lig_in_progress$sample_id)
 
-# remove samples that have been ligated more than once and still haven't succeeded
-multi_lig <- not_pass %>% 
-  filter(!is.na(ligation_id)) %>% 
+# how many samples are beyond ligation but not seq'd yet ####
+in_progress <- samples %>% 
+  filter(lig_date > "2018-01-01")
+samples <- samples %>% 
+  filter(!sample_id %in% in_progress)
+
+# how many samples have a digest planned ####
+dig_in_progress <- samples %>% 
+  filter(!is.na(digest_id) &  # has been planned in a ligation
+      is.na(dig_date)) # has not been ligated
+write.csv(dig_in_progress, file = paste("data/dig_in_progress_as_of_", Sys.Date(), ".csv", sep = ""))
+samples <- samples %>% 
+  filter(!sample_id %in% dig_in_progress$sample_id)
+
+# are any of the remaining samples multi-lig samples that have still failed?
+multi_lig <- samples %>% 
+  filter(!is.na(ligation_id), 
+    !is.na(lig_date)) %>% 
   group_by(sample_id) %>% 
   summarise(count = n()) %>% 
   filter(count > 1)
-not_pass <- not_pass %>% 
+write.csv(multi_lig, file = paste("data/multiple_lig_failures_as_of_", Sys.Date(), ".csv", sep = ""))
+samples <- samples %>% 
   filter(!sample_id %in% multi_lig$sample_id)
 
+# get the extract locations for the remaining samples so they can be rescued
 extr_loc <- lab %>% 
   tbl("extraction") %>% 
+  filter(sample_id %in% samples$sample_id) %>% 
   select(extraction_id, plate, well, gel) %>% 
   collect()
-not_pass <- left_join(not_pass, extr_loc, by = "extraction_id")
+samples <- left_join(samples, extr_loc, by = "extraction_id")
 
-write.csv(not_pass, file = "data/attempt_to_rescue.csv", row.names = F)
+write.csv(samples, file = paste("data/attempt_to_rescue_as_of_", Sys.Date(), ".csv", sep = ""), row.names = F)
