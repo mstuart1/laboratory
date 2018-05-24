89dadbd5c1c92bc9ca8222bd67d469a752488313
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-05-24 11:06:00 -0400

    using

--- scripts/changedb.R
+++ scripts/changedb.R
@@ -7,8 +7,8 @@ lab <- write_db("Laboratory")
 
 # pull in all table
 # extr <- lab %>% dbReadTable("extraction") %>% collect()
-# digs <- lab %>% dbReadTable("digest") %>% collect()
-ligs <- lab %>% dbReadTable("ligation") %>% collect()
+digs <- lab %>% dbReadTable("digest") %>% collect()
+# ligs <- lab %>% dbReadTable("ligation") %>% collect()
 
 # change <- extr %>%
 #   filter(extraction_id < "E0247", 
@@ -16,21 +16,34 @@ ligs <- lab %>% dbReadTable("ligation") %>% collect()
 #     !grepl("empty", notes)) %>% 
 #   mutate(notes = ifelse(is.na(notes), "empty", paste("empty, ", notes)))
 
-# change <- digs %>%
-# filter(digest_id >= "D4614") %>%
-# mutate(notes = ifelse(is.na(notes), "redigest planned for summer 2018", paste("redigest planned for summer 2018, ", notes)))
 
-change <- ligs %>%
-  filter(plate == "L3171-L3266" | plate == "L3651-L3746") %>%
-  arrange(ligation_id) %>% 
-  mutate(notes = NA, 
-    date = "2018-05-16", 
-    barcode_num = rep(1:48, 4))
+# get quants to calculate dig ng_in
+extr <- lab %>%
+  dbReadTable("extraction") %>%
+  filter(extraction_id %in% digs$extraction_id) %>%
+  select(extraction_id, quant) %>%
+  collect() %>%
+  rename(extr_quant = quant)
+digs <- left_join(digs, extr, by = "extraction_id")
+
+change <- digs %>%
+  filter(plate == "D4794-D4887") %>%
+  mutate(date = "2018-05-23",
+    ng_in = extr_quant * vol_in,
+    notes = ifelse(grepl("planned", notes), NA, notes)) 
+
+# change <- ligs %>%
+#   filter(plate == "L3171-L3266" | plate == "L3651-L3746") %>%
+#   arrange(ligation_id) %>% 
+#   mutate(notes = NA, 
+#     date = "2018-05-16", 
+#     barcode_num = rep(1:48, 4))
   # mutate(notes = ifelse(ligation_id >= "L3171", "ligation planned for January 2018", notes))
 
 # extr <- change_rows(extr, change, "extraction_id")
-# digs <- change_rows(digs, change, "digest_id")
-ligs <- change_rows(ligs, change, "ligation_id")
+digs <- change_rows(digs, change, "digest_id") %>% 
+  select(-extr_quant)
+# ligs <- change_rows(ligs, change, "ligation_id")
 # write the changes to the db
 ################### BE CAREFUL ########################################
 # dbWriteTable(lab, "extraction", extr, row.names = F, overwrite = T)
